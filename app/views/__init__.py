8e026154f6b600419d57012c24261436590d051d
rafael <barlera1989@gmail.com>
2021-03-22 16:33:01 -0300

    feature: readme and init page

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -11,33 +11,9 @@ bp_index = Blueprint('index', __name__)
 main_button_data = MainButton()
 polynomial_object = PolynomialAttributes()
 
-
-""" @bp_index.route("/", methods=['GET', 'POST'])
-def main_page():
-
-    if request.method == "GET":
-
-        input_data.polynomial_degree = request.form['input_a']
-        input_data.polynomial_coefficient = request.form['input_b']
-        input_data.k_value = request.form['input_c']
-        input_data.initial_value = request.form['input_d']
-        input_data.max_iterations = request.form['input_e']
-        input_data.tolerance = request.form['input_g']
-
-    if request.method == "POST":
-
-        if request.form.get("submit_f"):
-            main_button_data.button_page2 = True
-
-    print(input_data.polynomial_degree)
-    print(input_data.polynomial_coefficient)
-    print(input_data.k_value)
-    print(input_data.initial_value)
-    print(input_data.max_iterations)
-    print(input_data.tolerance)
-    print(main_button_data.button_page2)
-
-    return render_template('page2.html') """
+@bp_index.route("/", methods=['GET'])
+def home_page():
+    return render_template('index.html')
 
 
 @bp_index.route("/request", methods=['POST'])
@@ -52,11 +28,14 @@ def request_data():
         polynomial_object.max_iterations = int(data["max_iterations"])
         polynomial_object.tolerance = float(data["tolerance"])
 
-        response = {
-            "Eq_zero": optimize_data(polynomial_object)
-        }
+        try:
+            response = {
+                "Eq_zero": optimize_data(polynomial_object)
+            }
+        except:
+            response = {'message': 'zero not found'}
 
-        print(optimize_data(polynomial_object))
+        
 
     return response
 
